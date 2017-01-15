10c6dbd5dcb66874db386ed9074bbf0558612ca5
netanelz <netanelza100@walla.com>
2017-01-15 19:57:49 +0200

    the beginning work

---
+++ A17 Ex03/Ex03.GarageLogic/EngineRunsOnFuel.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ex03.GarageLogic
+{
+    class EngineRunsOnFuel : Engine
+    {
+        private readonly eFuelType r_EngineFuelType;
+
+        public EngineRunsOnFuel (eFuelType i_EngineFuelType, float i_MaxEngineCapacity) : base(i_MaxEngineCapacity)
+        {
+            r_EngineFuelType = i_EngineFuelType;
+        }
+
+        public eFuelType FuelType
+        {
+            get { return r_EngineFuelType; }
+        }
+
+        public void EngineFuelFill(eFuelType i_FuelType, string i_AmountToFillInEngine)
+        {
+            if (i_FuelType == r_EngineFuelType)
+            {
+                float amountToFillInEngine;
+                float amountOfRemainingLiters = MaxEnergy - CurrentEnergyInVehicle;
+                bool trueInput = float.TryParse(i_AmountToFillInEngine, out amountToFillInEngine);
+                if (trueInput)
+                {
+                    if (amountToFillInEngine < amountOfRemainingLiters)
+                    {
+                        CurrentEnergyInVehicle += amountToFillInEngine;
+                    }
+                    else
+                    {
+                        throw new ValueOutOfRangeException(0, MaxEnergy, "you are trying to fill above the max fuel tank");
+                    }
+                }
+                else
+                {
+                    throw new FormatException("invalid input, the input should be a number!!");
+                }
+            }
+            else
+            {
+                throw new ArgumentException("the fuel type is not correct");
+            }
+        }
+    }
+}
---
+++ A17 Ex03/Ex03.GarageLogic/ValueOutOfRangeException.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ex03.GarageLogic
+{
+    class ValueOutOfRangeException : Exception
+    {
+        private readonly float r_MinValue;
+        private readonly float r_MaxValue;
+        
+        public float MinValue
+        {
+            get { return r_MinValue; }
+        }
+        
+        public float MaxValue
+        {
+            get { return r_MaxValue; }
+        } 
+
+        public ValueOutOfRangeException(
+            float i_MinValue,
+            float i_MaxValue,
+            string i_Message)
+            : base (i_Message)
+        {
+            this.r_MinValue = i_MinValue;
+            this.r_MaxValue = i_MaxValue;
+        }
+    }
+}
